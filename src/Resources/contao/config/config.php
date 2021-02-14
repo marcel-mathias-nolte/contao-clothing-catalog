e7c17957076d468c61cdda2d51e01b7543e35cc0
marcel-mathias-nolte <marcel.nolte@noltecomputer.de>
2021-02-14 16:25:00 +0100

    Initial commit

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -11,7 +11,6 @@
 
 namespace MarcelMathiasNolte\ContaoClothingCatalogBundle;
 
-use Contao\ArrayUtil;
 
 /**
  * Hooks
@@ -21,12 +20,12 @@ use Contao\ArrayUtil;
  * Backend Modules
  */
 if (!isset($GLOBALS['BE_MOD']['clothing_catalog'])) {
-    ArrayUtil::arrayInsert($GLOBALS['BE_MOD'], 0, array(
+    \ArrayUtil::arrayInsert($GLOBALS['BE_MOD'], 0, array(
         'clothing_catalog' => array()
     ));
 }
 if (!isset($GLOBALS['BE_MOD']['clothing_catalog']['clothing_catalog_categories'])) {
-    ArrayUtil::arrayInsert($GLOBALS['BE_MOD']['clothing_catalog'], 3, array(
+    \ArrayUtil::arrayInsert($GLOBALS['BE_MOD']['clothing_catalog'], 3, array(
         'clothing_catalog_categories' => array(
             'tables' => array('tl_clothing_categories')
         ),
